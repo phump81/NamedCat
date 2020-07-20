1a461f14150ddded2bb337fc409dc644b7029a90
Paul Humphreys <paulhumphreys81@aol.co.uk>
2020-07-20 15:15:42 +0100

    first commit

---
+++ app.rb
@@ -0,0 +1,23 @@
+require 'sinatra'
+
+get '/' do
+  "Hello World"
+end
+
+get '/secret' do
+  "I'm not telling you"
+end
+
+get '/fruit' do
+  "banana"
+end
+
+get '/anotherfruit' do
+  "raspberry"
+end
+
+get '/cat' do
+  "<div style='border: 3px dashed red'>
+     <img src='http://bit.ly/1eze8aE'>
+   </div>"
+end
