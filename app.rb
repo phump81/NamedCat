7d27ef2745463d4fe849f0bcedee174140c3bbc1
Paul Humphreys <paulhumphreys81@aol.co.uk>
2020-07-20 17:21:13 +0100

    Posted Params

--- app.rb
+++ app.rb
@@ -12,7 +12,17 @@ get '/fruit' do
   "banana"
 end
 
-get '/cat' do
+get '/random-cat' do
   @name = ["Amigo", "Oscar", "Viking"].sample
   erb(:index)
+end
+
+post '/named-cat' do
+  p params
+  @name = params[:name]
+  erb(:index)
+end
+
+get '/cat-form' do
+  erb(:cat_form)
 end
